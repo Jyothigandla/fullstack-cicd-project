298d458251c710f64df3f8cf7e977db2816dcc94
jyothigandla <jyothigandla9@gmail.com>
2026-05-25 11:34:48 +0530

    initial frontend backend project

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/api/message", (req, res) => {
+    res.json({
+        message: "Backend API Working Successfully"
+    });
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(
+        path.join(__dirname,
+        "public",
+        "index.html")
+    );
+});
+
+app.listen(3000, () => {
+    console.log(
+        "Server running on port 3000"
+    );
+});
